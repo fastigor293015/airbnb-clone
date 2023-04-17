fe655aa09f0e898c2719ab7f4a153c67c7aeff11
Igor Shchetinkin <fastigor293015@gmail.com>
2023-04-17 23:13:48 +1000

    rolled back all changes (searchParams still don't work in prod)

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,13 @@ import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
 interface HomeProps {
-  params?: string;
-  searchParams?: IListingsParams;
+  searchParams: IListingsParams;
 }
 
 export const dynamic = "force-dynamic";
 
-const Home = async (params: HomeProps) => {
-  const listings = await getListings(params?.searchParams!);
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
